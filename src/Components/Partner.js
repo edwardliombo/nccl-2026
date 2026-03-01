68be43224a57cf44ebd19fffdfefba9edee11d40
Edward Liombo <edwardliombo@gmail.com>
2026-03-01 14:19:08 +0300

    changes to toggle

--- src/Components/Partner.js
+++ src/Components/Partner.js
@@ -1,13 +1,10 @@
 import { useState } from "react";
 const Partner = ({id, title, short, ico})=>{
     const [toggle, setToggle]=useState(false)
-    const changetext =()=>{
-        setToggle(!toggle)
-    }
+    
     return(
         <section className="flex flex-col items-center text-center ">
-            
-            <div onMouseEnter={changetext} className="md:flex md:flex-col items-center justify-center shadow-lg p-4 h-40  w-48">
+            <div onMouseEnter={()=>setToggle(true)} onMouseLeave={()=>setToggle(false)} className="md:flex md:flex-col items-center justify-center shadow-lg p-4 h-40  w-48">
                 <img width={70}  src={ico} alt={title} />
                 <h1  className="text-[10px] hover:transition-opacity font-bold">{toggle?title:short}</h1>
             </div>
