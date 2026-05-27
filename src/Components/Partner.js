fa405fc953e5365b6cff8d974bab40a2f0b8f302
Edward Liombo <edwardliombo@gmail.com>
2026-05-27 14:19:50 +0300

    CHANGES

--- src/Components/Partner.js
+++ src/Components/Partner.js
@@ -2,13 +2,10 @@
 const Partner = ({id, title, short, ico})=>{
     
     return(
-        <section  className="w-full h-full">
-            
+        <section  className="w-full h-full">  
             <div className="flex flex-col h-32 mix-blend-multiply hover:mix-blend-normal bg-white justify-center items-center text-center relative">
                 <img className='w-20 mix-blend-multiply  object-contain' src={ico} alt={title} />
             </div>
-
-            
         </section>
     )
 }
